0b7c2562a99898a05c0258acef26e86a845d9f23
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-06-19 18:24:50 -0700

    feat(GeneralInfo.jsx): General info form - Add basic JSX form for general info input

--- CV-Application/src/App.jsx
+++ CV-Application/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GeneralInfo from './components/generalInfo' 
 
 function App() {
 
 
   return (
     <>
+    <GeneralInfo />
      </>
   )
 }
---
+++ CV-Application/src/components/GeneralInfo.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function GeneralInfo() {
+    const [firstName,setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [email, setEmail] = useState()
+    const [phoneNumber, setPhoneNumber] = useState()
+    
+
+
+    return (
+        <>
+        <div className="generalForm">
+        <form>
+            <label>First Name:</label>
+            <input type="text" />
+
+            <label>Last Name:</label>
+            <input type="text" />
+
+            <label>Email:</label>
+            <input type="email" />
+
+            <label>Phone Number:</label>
+            <input type="tel" />       
+
+        </form>
+        </div>
+        </>
+    )
+}
