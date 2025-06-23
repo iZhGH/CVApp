e5a108685a3f7d617fbecef9c835ed22daf34344
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-06-23 11:42:30 -0700

    resumeContent html: - Add placeholder JSX content for resumeContent component

--- CV-Application/src/components/ResumeContent.jsx
+++ CV-Application/src/components/ResumeContent.jsx
@@ -11,6 +11,25 @@ export default function ResumeContent() {
             </ul>
             </div>
 
+            <div className="educationInformation">
+            <h1>Education</h1>
+            <h2>School</h2>
+            <div className="majorDateInfo">
+            <h4>Major</h4>
+            <p>Date</p>
+            </div>
+
+            </div>
+
+            <div className="experienceInformation">
+            <h1>Experience</h1>
+            <h2>Company</h2>
+            <div className="positionDateInfo">
+            <p>Position</p>
+            <p>Date</p>
+            </div>
+            </div>
+
         </div>
     )
 }
